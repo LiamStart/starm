f6e4b9e53126da3c033de5b1c05af9329c4efc12
Liam Star <49294846+LiamStart@users.noreply.github.com>
2020-05-10 15:37:00 -0500

    CAMBIO EN EL INDEX TODAVIA

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -17,8 +17,6 @@
                     </div>
                 </div>
 
-                <div class="col-12">
-                                        </div>
 
                 <div class="table-responsive">
                     <table class="table align-items-center table-flush">
